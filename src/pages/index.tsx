9649b1eecc72ab14600c704f28cb4e83d7b81255
Illia Kornyk <krekerbrow@gmail.com>
2023-07-02 16:18:12 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,16 @@ import { useEffect, useState } from 'react';
 
 import CarsTable from '@/components/carsTable';
 import Layout from '@/components/layout/Layout';
+import Pagination from '@/components/pagination';
 
 import { Car, getCars } from '../api';
+
+const CARS_PER_PAGE = 10;
 
 export default function HomePage() {
   const [cars, setCars] = useState<Car[]>([]);
   const [error, setError] = useState<string | null>(null);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     async function fetchCars() {
@@ -27,9 +31,19 @@ export default function HomePage() {
     return <div>Error: {error}</div>;
   }
 
+  const totalPages = Math.ceil(cars.length / CARS_PER_PAGE);
+  const startIndex = (currentPage - 1) * CARS_PER_PAGE;
+  const endIndex = startIndex + CARS_PER_PAGE;
+  const carsToDisplay = cars.slice(startIndex, endIndex);
+
   return (
     <Layout>
-      <CarsTable cars={cars} />
+      <CarsTable cars={carsToDisplay} />
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={setCurrentPage}
+      />
     </Layout>
   );
 }

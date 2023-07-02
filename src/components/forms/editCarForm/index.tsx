4ea2335e3e7b5f59d2c5d50beffb7a128d8e985e
Illia Kornyk <krekerbrow@gmail.com>
2023-07-02 17:35:22 +0300

    feat: connect React Hook Form
    Connect React Hook Form instead of usual form handling

--- src/components/forms/editCarForm/index.tsx
+++ src/components/forms/editCarForm/index.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 import Modal from '@/components/modal';
 
 import { Car } from '@/api';
-
-export interface EditCarFormProps {
+interface EditCarFormProps {
   car: Car;
   isOpen: boolean;
   onClose: () => void;
@@ -18,102 +17,114 @@ export default function EditCarForm({
   onClose,
   onSave,
 }: EditCarFormProps) {
-  const [color, setColor] = useState(car.car_color);
-  const [price, setPrice] = useState(car.price);
-  const [availability, setAvailability] = useState(car.availability);
+  const { register, handleSubmit, reset } = useForm({
+    defaultValues: {
+      color: car.car_color,
+      price: car.price,
+      availability: car.availability ? 'Yes' : 'No',
+    },
+  });
 
-  const handleSave = () => {
+  const onSubmit = (data: any) => {
     onSave({
       ...car,
-      car_color: color,
-      price,
-      availability,
+      car_color: data.color,
+      price: data.price,
+      availability: data.availability === 'Yes',
     });
     onClose();
   };
 
+  React.useEffect(() => {
+    reset({
+      color: car.car_color,
+      price: car.price,
+      availability: car.availability ? 'Yes' : 'No',
+    });
+  }, [car, reset]);
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <h2 className='mb-4 text-lg font-semibold'>Edit Car</h2>
-      <div className='mb-4'>
-        <label className='mb-1 block'>Company</label>
-        <input
-          className='w-full rounded border bg-gray-200 px-4 py-2'
-          type='text'
-          value={car.car}
-          disabled
-        />
-      </div>
-      <div className='mb-4'>
-        <label className='mb-1 block'>Model</label>
-        <input
-          className='w-full rounded border bg-gray-200 px-4 py-2'
-          type='text'
-          value={car.car_model}
-          disabled
-        />
-      </div>
-      <div className='mb-4'>
-        <label className='mb-1 block'>VIN</label>
-        <input
-          className='w-full rounded border bg-gray-200 px-4 py-2'
-          type='text'
-          value={car.car_vin}
-          disabled
-        />
-      </div>
-      <div className='mb-4'>
-        <label className='mb-1 block'>Year</label>
-        <input
-          className='w-full rounded border bg-gray-200 px-4 py-2'
-          type='text'
-          value={car.car_model_year}
-          disabled
-        />
-      </div>
-      <div className='mb-4'>
-        <label className='mb-1 block'>Color</label>
-        <input
-          className='w-full rounded border  px-4 py-2'
-          type='text'
-          value={color}
-          onChange={(event) => setColor(event.target.value)}
-        />
-      </div>
-      <div className='mb-4'>
-        <label className='mb-1 block'>Price</label>
-        <input
-          className='w-full rounded border px-4 py-2'
-          type='text'
-          value={price}
-          onChange={(event) => setPrice(event.target.value)}
-        />
-      </div>
-      <div className='mb-4'>
-        <label className='mb-1 block'>Availability</label>
-        <select
-          className='w-full rounded border px-4 py-2'
-          value={availability ? 'Yes' : 'No'}
-          onChange={(event) => setAvailability(event.target.value === 'Yes')}
-        >
-          <option>Yes</option>
-          <option>No</option>
-        </select>
-      </div>
-      <div className='flex justify-end'>
-        <button
-          className='mr-2 rounded bg-gray-100 px-4 py-2'
-          onClick={onClose}
-        >
-          Cancel
-        </button>
-        <button
-          className='rounded bg-blue-500 px-4 py-2 text-white'
-          onClick={handleSave}
-        >
-          Save
-        </button>
-      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className='mb-4'>
+          <label className='mb-1 block'>Company</label>
+          <input
+            className='w-full rounded border bg-gray-100 px-4 py-2'
+            type='text'
+            value={car.car}
+            disabled
+          />
+        </div>
+        <div className='mb-4'>
+          <label className='mb-1 block'>Model</label>
+          <input
+            className='w-full rounded border bg-gray-100 px-4 py-2'
+            type='text'
+            value={car.car_model}
+            disabled
+          />
+        </div>
+        <div className='mb-4'>
+          <label className='mb-1 block'>VIN</label>
+          <input
+            className='w-full rounded border bg-gray-100 px-4 py-2'
+            type='text'
+            value={car.car_vin}
+            disabled
+          />
+        </div>
+        <div className='mb-4'>
+          <label className='mb-1 block'>Year</label>
+          <input
+            className='w-full rounded border bg-gray-100 px-4 py-2'
+            type='text'
+            value={car.car_model_year}
+            disabled
+          />
+        </div>
+        <div className='mb-4'>
+          <label className='mb-1 block'>Color</label>
+          <input
+            {...register('color')}
+            className='w-full rounded border px-4 py-2'
+            type='text'
+          />
+        </div>
+        <div className='mb-4'>
+          <label className='mb-1 block'>Price</label>
+          <input
+            {...register('price')}
+            className='w-full rounded border px-4 py-2'
+            type='text'
+          />
+        </div>
+        <div className='mb-4'>
+          <label className='mb-1 block'>Availability</label>
+          <select
+            {...register('availability')}
+            className='w-full rounded border px-4 py-2'
+          >
+            <option>Yes</option>
+            <option>No</option>
+          </select>
+        </div>
+        <div className='flex justify-end'>
+          <button
+            type='button'
+            className='mr-2 rounded bg-gray-100 px-4 py-2'
+            onClick={onClose}
+          >
+            Cancel
+          </button>
+          <button
+            type='submit'
+            className='rounded bg-blue-500 px-4 py-2 text-white'
+          >
+            Save
+          </button>
+        </div>
+      </form>
     </Modal>
   );
 }

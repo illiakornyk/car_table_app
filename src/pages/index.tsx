bbf3cc657b329ceea117321208034c1cdaec7df5
Illia Kornyk <krekerbrow@gmail.com>
2023-07-02 14:15:47 +0300

    feat: add some basic `Layout` and car table

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 
 import Layout from '@/components/layout/Layout';
 
-import { Car, getCars } from '@/api';
+import { Car, getCars } from '../api';
 
 export default function HomePage() {
   const [cars, setCars] = useState<Car[]>([]);
@@ -26,5 +26,40 @@ export default function HomePage() {
     return <div>Error: {error}</div>;
   }
 
-  return <Layout>{/* Render cars here */}</Layout>;
+  return (
+    <Layout>
+      <div className='overflow-x-auto'>
+        <table className='w-full table-auto text-left'>
+          <thead>
+            <tr className='bg-gray-100'>
+              <th className='px-4 py-2'>Company</th>
+              <th className='px-4 py-2'>Model</th>
+              <th className='px-4 py-2'>VIN</th>
+              <th className='px-4 py-2'>Color</th>
+              <th className='px-4 py-2'>Year</th>
+              <th className='px-4 py-2'>Price</th>
+              <th className='px-4 py-2'>Availability</th>
+              <th className='px-4 py-2'>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cars.map((car) => (
+              <tr key={car.id}>
+                <td className='border px-4 py-2'>{car.car}</td>
+                <td className='border px-4 py-2'>{car.car_model}</td>
+                <td className='border px-4 py-2'>{car.car_vin}</td>
+                <td className='border px-4 py-2'>{car.car_color}</td>
+                <td className='border px-4 py-2'>{car.car_model_year}</td>
+                <td className='border px-4 py-2'>{car.price}</td>
+                <td className='border px-4 py-2'>
+                  {car.availability ? 'Yes' : 'No'}
+                </td>
+                <td className='border px-4 py-2'>Edit | Delete</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </Layout>
+  );
 }
